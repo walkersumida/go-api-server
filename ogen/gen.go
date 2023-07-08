47b758be4c3998c4f6c09d8cd324895a6608ff82
WalkerSumida <walkersumida@gmail.com>
2023-07-08 23:10:26 +0900

    move schema file

--- ogen/gen.go
+++ ogen/gen.go
@@ -1,3 +1,3 @@
 package ogen
 
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen --convenient-errors=on --target ./ --package ogen --clean ../schema.yml
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen --convenient-errors=on --target ./ --package ogen --clean ../api/schema.yml
