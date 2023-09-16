01589613473d1dabb0cbfcb1c759ca6c077a8109
WalkerSumida <walkersumida@gmail.com>
2023-09-16 11:06:28 +0900

    feat: print log of server stopped

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	"entgo.io/ent/dialect"
 	_ "github.com/lib/pq"
+	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
 	"github.com/walkersumida/go-api-server/database"
 	"github.com/walkersumida/go-api-server/ent"
 	"github.com/walkersumida/go-api-server/handler"
@@ -14,10 +20,24 @@ import (
 )
 
 func main() {
+	rootCmd := &cobra.Command{
+		Use:  "server",
+		Args: cobra.NoArgs,
+		RunE: func(_ *cobra.Command, _ []string) error {
+			return run()
+		},
+	}
+
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
+
+func run() error {
 	url := database.BuildDBURL(database.Configs{})
 	entCli, err := ent.Open(dialect.Postgres, url)
 	if err != nil {
-		log.Fatalf("failed opening connection to postgres: %v", err)
+		return errors.Wrap(err, "failed opening connection to postgres")
 	}
 	defer entCli.Close()
 
@@ -27,10 +47,37 @@ func main() {
 
 	srv, err := ogen.NewServer(h)
 	if err != nil {
-		log.Fatal(err)
+		return errors.Wrap(err, "failed to run server")
 	}
 
-	if err := http.ListenAndServe(":8080", srv); err != nil {
-		log.Fatal(err)
+	const timeout = 60 * time.Second
+	httpServer := http.Server{
+		Addr:         ":8080",
+		Handler:      srv,
+		ReadTimeout:  timeout,
+		WriteTimeout: timeout,
 	}
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
+	idleConnsClosed := make(chan struct{})
+	go func() {
+		<-ctx.Done()
+
+		if err := httpServer.Shutdown(context.Background()); err != nil {
+			log.Printf("HTTP server Shutdown: %v", err)
+		}
+		close(idleConnsClosed)
+	}()
+
+	log.Println("Server started")
+	if err := httpServer.ListenAndServe(); err != http.ErrServerClosed {
+		log.Printf("HTTP server ListenAndServe: %v", err)
+	}
+
+	<-idleConnsClosed
+	log.Println("Server stopped")
+
+	return nil
 }
